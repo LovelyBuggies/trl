4aac769b9f78fc976a1f7abb28aecfb096a79639
N!no <ninomyemail@gmail.com>
2025-05-11 21:26:38 -0400

    Update magrpo_trainer.py

--- trl/trainer/magrpo_trainer.py
+++ trl/trainer/magrpo_trainer.py
@@ -1201,9 +1201,9 @@ def example_usage_multi_reward():
     agents = []
     for _ in range(2):
         base_model = AutoModelForCausalLM.from_pretrained(model_name)
-        # lora_model = get_peft_model(base_model, lora_config)
-        # lora_model.print_trainable_parameters()
-        lora_model = base_model
+        lora_model = get_peft_model(base_model, lora_config)
+        lora_model.print_trainable_parameters()
+        # lora_model = base_model
         agents.append(lora_model)
 
     # Initialize trainer with multiple reward functions
